2e8fde5707ea5e62d913e68b13d98ea0fe1d1005
MiguelEWV <miguelwellspay1@gmail.com>
2023-04-26 21:44:44 -0300

    Correccion del link

--- Script_IaC_Servidor_Web_Apache.sh
+++ Script_IaC_Servidor_Web_Apache.sh
@@ -7,7 +7,7 @@ apt-get install apache2 unzip -y
 
 echo "Baixando e copiando os arquivos de aplicacao..."
 cd /tmp
-wget https://github.com/denilsonbonati/linux-site-dio/archive/refs/head/main.zip
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/ -y
